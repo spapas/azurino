711b25cde789a8349da3b77dac3b96cab256a929
Serafeim Papastefanos <spapas@gmail.com>
2026-01-27 14:22:58 +0200

    feat: Add gen_smtp dependency

--- mix.exs
+++ mix.exs
@@ -61,6 +61,7 @@ defmodule Azurino.MixProject do
        compile: false,
        depth: 1},
       {:swoosh, "~> 1.16"},
+      {:gen_smtp, "~> 1.2"},
       {:req, "~> 0.5"},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
